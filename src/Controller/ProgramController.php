b2d9675528624329d6ac716259889cdc657e9f88
julie-jeancon-donon <dononjulie@gmail.com>
2022-11-16 16:34:27 +0100

    quêtes routing

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,10 +9,19 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProgramController extends AbstractController
 {
     #[Route('/program/', name: 'program_index')]
+    
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Series',
+        ]);
+    }
+
+    #[Route('/program/{id}/', requirements: ['id'=>'\d+'], name: 'program_id')]
+    public function show(int $id): Response
+    {
+        return $this->render('program/show.html.twig', [
+            'id' => $id,
         ]);
     }
 }
